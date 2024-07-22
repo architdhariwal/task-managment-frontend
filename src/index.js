46bea0433d84db42d1a87fea2708f6b609ba6765
architKumar <https://github.com/architdhariwal>
2024-07-22 15:52:09 +0530

    corrected index.js

--- src/index.js
+++ src/index.js
@@ -27,9 +27,9 @@ root.render(
       <Router>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+          {/* <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}> */}
             <App />
-          </GoogleOAuthProvider>
+          {/* </GoogleOAuthProvider> */}
         </ThemeProvider>
       </Router>
     </Provider>
